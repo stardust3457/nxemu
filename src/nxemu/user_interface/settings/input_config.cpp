176ef08ce6416fb8f9e4abdfac5e26eb5e3e9c6e
N3xoX1 <n3xox1@nxemu.com>
2025-10-07 11:36:05 +1030

    UI: More work on the input config

--- src/nxemu/user_interface/settings/input_config.cpp
+++ src/nxemu/user_interface/settings/input_config.cpp
@@ -13,14 +13,10 @@ InputConfig::~InputConfig()
 
 void InputConfig::Display(void * parentWindow)
 {
-    enum
-    {
-        WINDOW_HEIGHT = 300,
-        WINDOW_WIDTH = 300,
-    };
-
-    if (!m_sciterUI.WindowCreate(parentWindow, "input_config.html", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, SUIW_CHILD, m_window))
+    if (!m_sciterUI.WindowCreate(parentWindow, "input_config.html", 0, 0, 300, 300, SUIW_CHILD, m_window))
     {
         return;
     }
+    m_window->FixMinSize();
+    m_window->CenterWindow();
 }
