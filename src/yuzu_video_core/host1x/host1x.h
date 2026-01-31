ac498115c2b8dae6b7a79c5dea23fd9ac75de387
N3xoX1 <n3xox1@nxemu.com>
2026-01-31 20:12:29 +1030

    Video: some code cleanup

--- src/yuzu_video_core/host1x/host1x.h
+++ src/yuzu_video_core/host1x/host1x.h
@@ -11,48 +11,60 @@
 #include "yuzu_video_core/memory_manager.h"
 #include <nxemu-module-spec/operating_system.h>
 
-namespace Core {
+namespace Core
+{
 class System;
 } // namespace Core
 
-namespace Tegra {
+namespace Tegra
+{
 
-namespace Host1x {
+namespace Host1x
+{
 
-class Host1x {
+class Host1x
+{
 public:
     explicit Host1x(IDeviceMemory & deviceMemory);
     ~Host1x();
 
-    SyncpointManager& GetSyncpointManager() {
+    SyncpointManager & GetSyncpointManager()
+    {
         return syncpoint_manager;
     }
 
-    const SyncpointManager& GetSyncpointManager() const {
+    const SyncpointManager & GetSyncpointManager() const
+    {
         return syncpoint_manager;
     }
 
-    Tegra::MaxwellDeviceMemoryManager& MemoryManager() {
+    Tegra::MaxwellDeviceMemoryManager & MemoryManager()
+    {
         return memory_manager;
     }
 
-    const Tegra::MaxwellDeviceMemoryManager& MemoryManager() const {
+    const Tegra::MaxwellDeviceMemoryManager & MemoryManager() const
+    {
         return memory_manager;
     }
 
-    Tegra::MemoryManager& GMMU() {
+    Tegra::MemoryManager & GMMU()
+    {
         return gmmu_manager;
     }
 
-    const Tegra::MemoryManager& GMMU() const {
+    const Tegra::MemoryManager & GMMU() const
+    {
         return gmmu_manager;
     }
 
-    Common::FlatAllocator<u32, 0, 32>& Allocator() {
+    Common::FlatAllocator<u32, 0, 32> & Allocator()
+    {
         return *allocator;
     }
 
-    const Common::FlatAllocator<u32, 0, 32>& Allocator() const {
+    const Common::FlatAllocator<u32, 0, 32> & Allocator() const
+    {
         return *allocator;
     }
 
