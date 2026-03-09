95697b6fa8ac33c1bc4519f6dc051c3e606f43cb
N3xoX1 <n3xox1@nxemu.com>
2026-03-09 21:46:21 +1030

    Common: remove usage of fixed path buffers

--- src/common/path.cpp
+++ src/common/path.cpp
@@ -159,58 +159,57 @@ std::string Path::GetExtension(void) const
 
 void Path::GetComponents(std::string * drive, std::string * directory, std::string * name, std::string * extension) const
 {
-    char driveBuff[_MAX_DRIVE + 1] = {0}, dirBuff[_MAX_DIR + 1] = {0}, nameBuff[_MAX_FNAME + 1] = {0}, extBuff[_MAX_EXT + 1] = {0};
-
+    std::string drivePart, dirPart, namePart, extPart;
     const char * basePath = m_path.c_str();
-    const char * driveDir = strrchr(basePath, DRIVE_DELIMITER[0]);
-    if (driveDir != nullptr)
+#ifdef WIN32
+    const char * driveDelim = strrchr(basePath, DRIVE_DELIMITER[0]);
+    if (driveDelim != nullptr)
     {
-        size_t len = sizeof(dirBuff) < (driveDir - basePath) ? sizeof(driveBuff) : driveDir - basePath;
-        strncpy(driveBuff, basePath, len);
-        basePath += len + 1;
+        drivePart.assign(basePath, driveDelim - basePath);
+        basePath = driveDelim + 1;
     }
+#endif
 
-    const char * last = strrchr(basePath, DIRECTORY_DELIMITER[0]);
-    if (last != nullptr)
+    const char * lastSep = strrchr(basePath, DIRECTORY_DELIMITER[0]);
+    if (lastSep != nullptr)
     {
-        size_t len = sizeof(dirBuff) < (last - basePath) ? sizeof(dirBuff) : last - basePath;
-        if (len > 0)
+        if (lastSep > basePath)
         {
-            strncpy(dirBuff, basePath, len);
+            dirPart.assign(basePath, lastSep - basePath);
         }
         else
         {
-            dirBuff[0] = DIRECTORY_DELIMITER[0];
-            dirBuff[1] = '\0';
+            dirPart = DIRECTORY_DELIMITER;
         }
-        strncpy(nameBuff, last + 1, sizeof(nameBuff));
+        namePart = lastSep + 1;
     }
     else
     {
-        strncpy(dirBuff, basePath, sizeof(dirBuff));
+        namePart = basePath;
     }
-    char * ext = strrchr(nameBuff, '.');
+
+    const char * ext = strrchr(namePart.c_str(), '.');
     if (ext != nullptr)
     {
-        strncpy(extBuff, ext + 1, sizeof(extBuff));
-        *ext = '\0';
+        extPart = ext + 1;
+        namePart.resize(ext - namePart.c_str());
     }
 
     if (drive)
     {
-        *drive = driveBuff;
+        *drive = drivePart;
     }
     if (directory)
     {
-        *directory = dirBuff;
+        *directory = dirPart;
     }
     if (name)
     {
-        *name = nameBuff;
+        *name = namePart;
     }
     if (extension)
     {
-        *extension = extBuff;
+        *extension = extPart;
     }
 }
 
@@ -266,17 +265,43 @@ void Path::SetNameExtension(const char * nameExtension)
 
 void Path::SetComponents(const char * drive, const char * directory, const char * name, const char * extension)
 {
-    char fullname[MAX_PATH];
-
-    memset(fullname, 0, sizeof(fullname));
     if (directory == nullptr || strlen(directory) == 0)
     {
         static char emptyDir[] = {DIRECTORY_DELIMITER[0], '\0'};
         directory = emptyDir;
     }
 
-    _makepath(fullname, drive, directory, name, extension);
-    m_path = fullname;
+    std::string result;
+
+#ifdef _WIN32
+    if (drive != nullptr && strlen(drive) > 0)
+    {
+        result += drive;
+        result += DRIVE_DELIMITER;
+    }
+#endif
+
+    result += directory;
+    if (!result.empty() && result.back() != DIRECTORY_DELIMITER[0])
+    {
+        result += DIRECTORY_DELIMITER;
+    }
+    if (name != nullptr && strlen(name) > 0)
+    {
+        if (!result.empty() && result.back() != DIRECTORY_DELIMITER[0])
+        {
+            result += DIRECTORY_DELIMITER;
+        }
+        result += name;
+
+        if (extension != nullptr && strlen(extension) > 0)
+        {
+            result += EXTENSION_DELIMITER;
+            result += extension;
+        }
+    }
+
+    m_path = result;
 }
 
 void Path::AppendDirectory(const char * subDirectory)
