bb794e62076110e08f2a98b45cb19975cafd306b
N3xoX1 <n3xox1@nxemu.com>
2026-04-08 21:00:04 +0930

    Commons: get dynamic_library to support non WIN32

--- src/common/dynamic_library.cpp
+++ src/common/dynamic_library.cpp
@@ -1,5 +1,9 @@
 #include "dynamic_library.h"
+#ifdef _WIN32
 #include <windows.h>
+#else
+#include <dlfcn.h>
+#endif
 
 DynLibHandle DynamicLibraryOpen(const char * path, bool showErrors)
 {
@@ -7,18 +11,29 @@ DynLibHandle DynamicLibraryOpen(const char * path, bool showErrors)
     {
         return nullptr;
     }
+#ifdef _WIN32
     UINT lastErrorMode = SetErrorMode(showErrors ? 0 : SEM_FAILCRITICALERRORS);
     DynLibHandle lib = (DynLibHandle)LoadLibraryA(path);
     SetErrorMode(lastErrorMode);
+#else
+    (void)showErrors;
+    DynLibHandle Lib = (DynLibHandle)(dlopen(path, RTLD_NOW | RTLD_LOCAL));
+#endif
     return lib;
 }
 
 void DynamicLibraryClose(DynLibHandle libHandle)
 {
-    if (libHandle != nullptr)
+    if (libHandle == nullptr)
     {
-        FreeLibrary((HMODULE)libHandle);
+        return;
     }
+
+#ifdef _WIN32
+    FreeLibrary((HMODULE)(libHandle));
+#else
+    dlclose(libHandle);
+#endif
 }
 
 void * DynamicLibraryGetProc(DynLibHandle libHandle, const char * name)
@@ -27,5 +42,10 @@ void * DynamicLibraryGetProc(DynLibHandle libHandle, const char * name)
     {
         return nullptr;
     }
+
+#ifdef _WIN32
     return GetProcAddress((HMODULE)libHandle, name);
+#else
+    return dlsym(libHandle, name);
+#endif
 }
