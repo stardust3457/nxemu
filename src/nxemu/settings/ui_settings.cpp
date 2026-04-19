7f38c1c836b6c7cd2dd406d47a8c29af75cb5198
Stardust <stardust3457@gmail.com>
2026-04-20 07:04:07 +0930

    Add hotkey for stop emulation, controller and configuration

--- src/nxemu/settings/ui_settings.cpp
+++ src/nxemu/settings/ui_settings.cpp
@@ -17,6 +17,9 @@ namespace
         {Hotkey::Fullscreen, {.key = (uint32_t)SCITER_KEY_F11, .ctrl = false, .alt = false, .shift = false}},
         {Hotkey::ExitFullscreen, {.key = (uint32_t)SCITER_KEY_ESCAPE, .ctrl = false, .alt = false, .shift = false}},
         {Hotkey::PauseContinue, {.key = (uint32_t)SCITER_KEY_F4}},
+        {Hotkey::StopEmulation, {.key = (uint32_t)SCITER_KEY_F5}},
+        {Hotkey::Controllers, {.key = (uint32_t)SCITER_KEY_C, .ctrl = true}},
+        {Hotkey::Configure, {.key = (uint32_t)SCITER_KEY_T, .ctrl = true}},
     };
 
     void merge_default_hotkeys(HotkeyMap & map)
